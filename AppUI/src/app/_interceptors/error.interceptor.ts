b22bc56c11553c6d1d1f70d37595caae536fe864
rsayana79 <rsayana79@gmail.com>
2022-05-26 07:41:43 -0400

    Redirected user to login upon unauthorized error

--- AppUI/src/app/_interceptors/error.interceptor.ts
+++ AppUI/src/app/_interceptors/error.interceptor.ts
@@ -37,6 +37,7 @@ export class ErrorInterceptor implements HttpInterceptor {
               break;
             case 401:
               this.toastr.error(error.statusText, error.status);
+              this.router.navigateByUrl('');
               break;
             case 404:
               this.router.navigateByUrl('/not-found');
